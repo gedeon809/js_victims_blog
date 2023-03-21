be2b3ef3de8b0bd6abda1ca4b544cf771db10363
Yuvraj Bhonsle <coding.fun103@gmail.com>
2023-03-21 22:06:14 +0530

    Login files added

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../index.css";
+
+export const Footer = () => {
+  return (
+    <footer className="text-center font-bold mb-4">
+      <h3>
+        <span className="text-xl">&copy;</span> <a href="#">JS Victims.org</a>
+      </h3>
+    </footer>
+  );
+};
